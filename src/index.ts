d0e1e8172f29b8a087067ddab07ff76982a20968
Luis Mori Guerra <luismorith@gmail.com>
2024-07-01 22:26:13 -0500

    feat: adding streaming example
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
 
 import { Ai } from '@cloudflare/ai';
+import streamingTemplate from "./streaming.html";
 
 type Bindings = {
 	AI: Ai;
@@ -10,32 +11,53 @@ import { Hono } from 'hono';
 const model = '@cf/mistral/mistral-7b-instruct-v0.1';
 
 const app = new Hono<{ Bindings: Bindings }>();
+app.get("/", (c) => c.html(streamingTemplate));
 
-app.get('/chat', async (ctx) => {
+app.get('/stream', async c => {
+	const ai = new Ai(c.env.AI);
+	const query = c.req.query('query');
+	const question = query || 'what is the origin of the phrase hello, world?';
 
-	const content = ctx.req.query('query') || 'what is the origin of the phrase hello, world?';
-
-	const ai = new Ai(ctx.env.AI);
 	const messages = [
 		{
 			role: 'system', content: ' you are a friendly assistant',
 		},
 		{
-			role: 'user', content,
+			role: 'user', content: question,
 		}
-	];
+	]
 
-	const inputs = { messages };
+	const aiResponse: any = await ai.run('@cf/meta/llama-2-7b-chat-int8', { messages, stream: true });
+
+	return new Response(aiResponse, {
+		headers: {
+			'Content-Type': 'text/event-stream',
+		}
+	})
+
+});
+
+app.get('/api/chat', async (ctx) => {
+
+	const content = ctx.req.query('query') || 'what is the origin of the phrase hello, world?';
+
+	const ai = new Ai(ctx.env.AI);
+
+	const inputs = {
+		messages: [
+			{
+				role: 'system', content: ' you are a friendly assistant',
+			},
+			{
+				role: 'user', content,
+			}
+		]
+	};
 
 	const res = await ai.run(model, inputs);
 
 	return ctx.json(res);
 
 });
-
-
-app.get('/', ctx => {
-	return ctx.json({ message: 'Hello World!' });
-})
 
 export default app
